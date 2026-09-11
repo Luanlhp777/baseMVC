0af6f788ec7f2562c3abba44f96b4bb946b093f2
Aluno <e023aluno@outlook.com>
2026-09-10 22:09:52 -0300

    Novo projeto MVC completo

---
+++ models/ClienteModel.js
@@ -0,0 +1,22 @@
+const db = require('../config/db');
+
+const Cliente = {
+
+    createNewClient: async(nome, cpf) => {
+        try {
+            const [result] = await db.execute('INSERT INTO Cliente (nome, cpf) VALUES(?, ?)', [nome, cpf]);
+
+            return { id: result.insertId, nome, cpf }
+        } catch (error) {
+            throw error;
+        }
+    },
+    getAllClients: async ()=>{
+        try {
+            const [rows] = await db.query('SELECT * FROM Cliente');
+            return rows;
+        } catch (error) {
+            throw error;
+        }
+    },
+}
